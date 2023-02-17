d167c2bdaced0efb62b1919708519d5eedee0e39
Jinxin Ai <ash30kw@icloud.com>
2023-02-17 20:33:10 +0100

    add index.js

--- jsx/src/App.js
+++ jsx/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  const name = 'Jinxin Ai'
+  const inputType = "number"
+  const minValue = 5;
+  //return <h1>{new Date().toLocaleDateString()}</h1>;
+  // return <input type={inputType} min={minValue}></input>
+  // return <textarea autoFocus={true}/>
+  // return <input maxLength={5}></input>
+  // return <input spellCheck={false}></input>
+  return <input style={{border: '1px solid blue'}}></input>
 }
 
 export default App;
--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,17 +1,9 @@
 // 1) Import the react and react dom libraries
-import React from 'react'
-import ReactDom from 'react-dom/client'
+import React from 'react';
+import ReactDom from 'react-dom/client';
+import App from './App';
+
 const el = document.getElementById('root')
 const root = ReactDom.createRoot(el)
-function App(){
-  const name = 'Jinxin Ai'
-  const inputType = "number"
-  const minValue = 5;
-  //return <h1>{new Date().toLocaleDateString()}</h1>;
-  // return <input type={inputType} min={minValue}></input>
-  // return <textarea autoFocus={true}/>
-  // return <input maxLength={5}></input>
-  // return <input spellCheck={false}></input>
-  return <input style={{border: '1px solid blue'}}></input>
-}
+
 root.render(<App/>)
