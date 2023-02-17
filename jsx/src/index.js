8ab4b78bcf5797c347caa93a0cf8638017d6398d
Jinxin Ai <ash30kw@icloud.com>
2023-02-17 19:49:30 +0100

    textarea

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,18 +1,14 @@
 // 1) Import the react and react dom libraries
-
 import React from 'react'
 import ReactDom from 'react-dom/client'
-
 const el = document.getElementById('root')
-
 const root = ReactDom.createRoot(el)
-
 function App(){
   const name = 'Jinxin Ai'
   const inputType = "number"
   const minValue = 5;
   //return <h1>{new Date().toLocaleDateString()}</h1>;
-  return <input type={inputType} min={minValue}></input>
+  // return <input type={inputType} min={minValue}></input>
+  return <textarea autoFocus={true}/>
 }
-
 root.render(<App/>)
